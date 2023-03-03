df98fbb87cf22074f11cf7989dcb9555917c7a78
a-rust <6aywagh8k@mozmail.com>
2023-03-02 17:54:44 -0800

    Testing api functions

--- src/main.py
+++ src/main.py
@@ -8,7 +8,17 @@ coin_gecko_baseurl = 'https://api.coingecko.com/api/v3/'
 coin_gecko_endpoint = 'search/trending'
 
 
+# Testing to see if everything works
+
 # Outputs a list of the top 7 most searched cryptocurrencies within the last 24 hours
 entire_request = api_request(coin_gecko_baseurl, coin_gecko_endpoint)
-list = order_by_searches(entire_request)
-print(list)
+ordered_list = order_by_searches(entire_request)
+print(ordered_list)
+
+# Outputs the price of each cryptocurrency in BTC
+price_in_btc = get_price_in_btc(entire_request)
+print(price_in_btc)
+
+# Outputs the price of each cryptocurrency in USD
+price_in_usd = compute_usd_price(entire_request)
+print(price_in_usd)
